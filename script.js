d4b5a259c30ff30f043ced771a9fdfc7e1125c8d
Borenkov Maksim <borenkovmv@gmail.com>
2024-01-13 21:20:46 +0300

    The and game pigs

--- script.js
+++ script.js
@@ -15,16 +15,35 @@ const current1Label = document.getElementById('current--1');
 const player0 = document.querySelector('.player--0');
 const player1 = document.querySelector('.player--1');
 
-score0Element.textContent = 0;
-score1Element.textContent = 0;
-diceElement.classList.add('hidden');
+// score0Element.textContent = 0;
+// score1Element.textContent = 0;
+// diceElement.classList.add('hidden');
 
-const totalScore = [0, 0];
-let currentScore = 0;
-let activePlyer = 0;
-let isPlays = true;
+let totalScore;
+let currentScore;
+let activePlyer;
+let isPlay;
 
-const switchActivePlay = () => {
+const resetGame = () => {
+  totalScore = [0, 0];
+  currentScore = 0;
+  activePlyer = 0;
+  isPlay = true;
+
+  score0Element.textContent = 0;
+  score1Element.textContent = 0;
+  current0Label.textContent = 0;
+  current0Label.textContent = 0;
+  player0.classList.remove('player--winner');
+  player1.classList.remove('player--winner');
+  player0.classList.add('player--active');
+  player1.classList.add('player--active');
+  diceElement.classList.add('hidden');
+};
+
+resetGame();
+
+const swoActivePlyer = () => {
   currentScore = 0;
   document.getElementById(`current--${activePlyer}`).textContent = currentScore;
   activePlyer = activePlyer === 0 ? 1 : 0;
@@ -32,87 +51,46 @@ const switchActivePlay = () => {
   player1.classList.toggle('player--active');
 };
 
-const roll = () => {
-  if (isPlays) {
-    //1.Random generate
-    const diceNumber = Math.trunc(Math.random() * 6) + 1;
+btnRoll.addEventListener('click', function () {
+  if (isPlay) {
+    const numberRandom = Math.trunc(Math.random() * 6) + 1;
+    console.log(numberRandom);
 
-    console.log(diceNumber);
-    //2.Display dice number
     diceElement.classList.remove('hidden');
-    diceElement.src = `dice${diceNumber}.png`;
+    diceElement.src = `dice${numberRandom}.png`;
 
-    //3. Later player no 1
-    if (diceNumber !== 1) {
-      currentScore += diceNumber;
+    if (numberRandom !== 1) {
+      currentScore += numberRandom;
+
       document.getElementById(`current--${activePlyer}`).textContent =
         currentScore;
-      // current0Label.textContent = currentScore;
     } else {
-      switchActivePlay();
+      swoActivePlyer();
     }
-    //  activePlyer === player0 ? 'player--active' :
-    // if (activePlyer) {
-    //   player0.classList.remove('player--active');
-    //   player1.classList.add('player--active');
-    // } else {
-    //   player1.classList.remove('player--active');
-    //   player0.classList.add('player--active');
-    // }
   }
-};
+});
 
-btnRoll.addEventListener('click', roll);
-
-const playHold = () => {
-  if (isPlays) {
-    //1.Add
+btnHold.addEventListener('click', function () {
+  if (isPlay) {
     totalScore[activePlyer] += currentScore;
+
     document.getElementById(`score--${activePlyer}`).textContent =
       totalScore[activePlyer];
-    //2
+
     if (totalScore[activePlyer] >= 20) {
-      isPlays = false;
+      isPlay = false;
       document
         .querySelector(`.player--${activePlyer}`)
         .classList.add('player--winner');
       document
         .querySelector(`.player--${activePlyer}`)
         .classList.remove('player--active');
-      diceElement.classList.add('hidden');
-      // btnHold.classList.add('stop');
     } else {
-      switchActivePlay();
+      swoActivePlyer();
     }
   }
-};
+});
 
-btnHold.addEventListener('click', playHold);
+btnNew.addEventListener('click', resetGame);
 
-const newGame = () => {
-  score0Element.textContent = 0;
-  score1Element.textContent = 0;
-  current0Label.textContent = 0;
-  current1Label.textContent = 0;
-  isPlays = true;
-  currentScore = 0;
-  activePlyer = 0;
-  // totalScore = [0, 0];
-  player0.classList.remove('player--winner');
-  player1.classList.remove('player--winner');
-  player0.classList.add('player--active');
-  diceElement.classList.add('hidden');
-  // btnHold.classList.remove('stop');
-  // document
-  //   .querySelector(`.player--${activePlyer}`)
-  //   .classList.remove('player--winner');
-  // document
-  //   .querySelector(`.player--${activePlyer}`)
-  //   .classList.add('player--active');
-};
-btnNew.addEventListener('click', newGame);
-
-// const newGame = () => {};
-
-// btnNew.addEventListener('click', newGame);
-5 / 85;
+5 / 89;
